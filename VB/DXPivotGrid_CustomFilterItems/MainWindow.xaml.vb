66c5b2241f11fc309f301c07b4eb7b4763073e3c
DevExpressExampleBot <fakeEmail>
2021-09-01 18:06:17 +0300

    autogenerate VB

--- VB/DXPivotGrid_CustomFilterItems/MainWindow.xaml.vb
+++ VB/DXPivotGrid_CustomFilterItems/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Windows
+﻿Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 Imports DevExpress.XtraPivotGrid.Data
 Imports DXPivotGrid_CustomFilterItems.DataSet1TableAdapters
@@ -7,15 +6,16 @@ Imports DXPivotGrid_CustomFilterItems.DataSet1TableAdapters
 Namespace DXPivotGrid_CustomFilterItems
 	Partial Public Class MainWindow
 		Inherits Window
-		Private productReportsDataTable As New DataSet1.ProductReportsDataTable()
+
+'INSTANT VB NOTE: The variable productReportsDataTable was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
+		Private productReportsDataTable_Conflict As New DataSet1.ProductReportsDataTable()
 		Private productReportsDataAdapter As New ProductReportsTableAdapter()
 		Public Sub New()
 			InitializeComponent()
 			pivotGridControl1.DataSource = productReportsDataAdapter.GetData()
 		End Sub
 		Private ReadOnly dummyItem As String = ""
-		Private Sub pivotGridControl1_CustomFilterPopupItems(ByVal sender As Object, _
-					ByVal e As PivotCustomFilterPopupItemsEventArgs)
+		Private Sub pivotGridControl1_CustomFilterPopupItems(ByVal sender As Object, ByVal e As PivotCustomFilterPopupItemsEventArgs)
 			If Object.ReferenceEquals(e.Field, fieldCategoryName) Then
 				For i As Integer = e.Items.Count - 1 To 0 Step -1
 					If Object.Equals(e.Items(i).Value, "Beverages") Then
@@ -23,9 +23,7 @@ Namespace DXPivotGrid_CustomFilterItems
 						Exit For
 					End If
 				Next i
-				e.Items.Insert(0, New PivotGridFilterItem(dummyItem, _
-							"Dummy Item", _
-							e.Field.FilterValues.Contains(dummyItem)))
+				e.Items.Insert(0, New PivotGridFilterItem(dummyItem, "Dummy Item", e.Field.FilterValues.Contains(dummyItem)))
 			End If
 		End Sub
 	End Class
